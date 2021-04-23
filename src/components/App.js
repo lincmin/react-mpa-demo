dde9a02f2b39080a7e8f5df84b0feea9f4ca51db
lincmin <lincm69696@qq.com>
2021-04-23 18:50:12 +0800

    使用 ahook useRequest

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
 import React, { useEffect } from "react";
+import { useRequest } from "ahooks";
 import axios from 'axios';
 import logo from '../logo.svg';
 import './App.css';
 import { Button } from 'antd';
 
-function App() {
-  useEffect(() => {
-    console.log("useEffect");
-    axios.get('/v1/goods/list?name=&sn=&limit=1&offset=0')
+function getList() {
+  return axios.get('/v1/goods/list?name=&sn=&limit=1&offset=0')
                 .then(function(response) {
-                    console.log(response)
+                    console.log(response.data)
+                    return response
                 })
                 .catch(function(error) {
                     console.log(error)
                 })
-  }, []);
+}
+
+function App() {
+  // useEffect(() => {
+  //   console.log("useEffect");
+  //   axios.get('/v1/goods/list?name=&sn=&limit=1&offset=0')
+  //               .then(function(response) {
+  //                   console.log(response)
+  //               })
+  //               .catch(function(error) {
+  //                   console.log(error)
+  //               })
+  // }, []);
+  const { data, error, loading } = useRequest(getList);
+  console.log('data',data)
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +37,7 @@ function App() {
         <Button type="primary">Button</Button>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>data{data}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
