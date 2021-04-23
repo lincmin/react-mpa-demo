848c87d289773f8f39b408773403e73e67009413
lincmin <lincm69696@qq.com>
2021-04-23 15:10:49 +0800

    使用 antd

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import logo from '../logo.svg';
 import './App.css';
-
+import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+        <Button type="primary">Button</Button>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
